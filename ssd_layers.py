21e296f6def212643150b08893f2834f2a27e94b
alan840706 <58114247+alan840706@users.noreply.github.com>
2019-12-12 01:08:37 +0800

    Update ssd_layers.py

--- ssd_layers.py
+++ ssd_layers.py
@@ -45,10 +45,11 @@ class Normalize(Layer):
 
     def call(self, x, mask=None):
         output = K.l2_normalize(x, self.axis)
+        print("//////////////////////////////////////////1")
         print(output)
-        print("////////////////////////////////////////")
-        print(self.gamma)
         print("////////////////////////////////////////2")
+        print(self.gamma)
+        print("////////////////////////////////////////3")
         
         output2 = output*self.gamma
         return output2
